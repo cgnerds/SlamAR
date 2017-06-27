58f51a3ac6a38e303a5d195cabbbcabe8fd20595
cgnerds <cgnerds@gmail.com>
2017-06-27 23:53:09 +0800

    The header file of class VisualOdometry.

---
+++ include/visual_odometry.h
@@ -0,0 +1,67 @@
+#ifndef VISUALODOMETRY_H
+#define VISUALODOMETRY_H
+
+#include "common_include.h"
+#include "map.h"
+
+#include <opencv2/features2d/features2d.hpp>
+
+namespace slamar
+{
+class VisualOdometry
+{
+public:
+	typedef shared_ptr<VisualOdometry> Ptr;
+	enum VOState {
+		INITIALIZING = -1,
+		OK = 0,
+		LOST
+	};
+
+	VOState state_;   // current VO status
+	Map::Ptr map_;    // map with all frames and map points
+	Frame::Ptr ref_;  // reference frame
+	Frame::Ptr curr_; // current frame
+
+	cv::Ptr<cv::ORB> orb_;                  // orb detector and computer
+	vector<cv::Point3f> pts_3d_ref_;        // 3d points in reference frame
+	vector<cv::KeyPoint> keypoints_curr_;   // keypoints in current frame
+	Mat                  descriptors_curr_; // descriptor in current frame
+	Mat                  descriptors_ref_;  // descriptor in reference frame
+	vecotr<cv::DMatch>   feature_matches_;
+
+	SE3 T_c_r_estimated_; // the estimated pose of current frame
+	int num_inliers_;     // number of inliers features in icp
+	int num_lost_;        // number of lost times
+	
+	// parameters
+	int num_of_features_; // number of features
+	double scale_factor_; // scale in image pyramid
+	int level_pyramid_;   // number of pyramid levels
+	float match_ratio_;   // ratio for selecting good matches
+	int max_num_lost_;    // max number of continuous lost times
+	int min_inliers;      // minimum inliers
+
+	double key_frame_min_rot;   // minimal rotation of two key-frames
+	double key_frame_min_trans; // minimal translations of two key-frames
+
+public:
+	VisualOdometry();
+	~VisualOdometry();
+
+	bool addFrame(Frame::Ptr frame); // add a new frame
+
+protected:
+	// inner opertation
+	void extractKeyPoints();
+	void computeDescriptors();
+	void featureMatching();
+	void poseEstimationPnP();
+	void setRef3DPoints();
+
+	void addKeyFrame();
+	bool checkEstimatedPose();
+	bool checkKeyFrame();
+};
+}
+#endif // !VISUALODOMETRY_H
