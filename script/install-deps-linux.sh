d2cbb785dce7b1689ed93da789f352ac917ca0ba
cgnerds <cgnerds@gmail.com>
2017-07-01 16:18:16 +0800

    Remove install-deps-linux.sh and change some Travis related settings.

--- script/install-deps-linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -ev
-sudo apt-get update -qq
-sudo apt-get install -qq build-essential libgtk2.0-dev libvtk5-dev libjpeg-dev libtiff4-dev libjasper-dev libopenexr-dev libtbb-dev qtdeclarative5-dev qt5-qmake libqglviewer-dev libeigen3-dev libsuitesparse-dev
